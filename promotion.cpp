585670ea0b77a136909e912d1b0d4b080c6ecb0d
Görkem Say <gorkemsay@protonmail.com>
2023-12-29 15:43:28 +0300

    added some code

--- promotion.cpp
+++ promotion.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 class Employee{
@@ -15,28 +14,24 @@ public:
     string getName(){
         return Name;
     }
-
     void setDepartment(string department){
         Department = department;
     }
     string getDepartment(){
         return Department;
     }
-
     void setRelation(string relation){
         Relation = relation;
     }
     string getRelation(){
         return Relation;
     }
-
     void setExperience(int experience){
         Experience = experience;
     }
     int getExperience(){
         return Experience;
     }
-
     void setAge(int age){
         if (age >= 18) {
             Age = age;
@@ -45,5 +40,31 @@ public:
     int getAge(){
         return Age;
     }
+    void staffInformation(){
+        cout << "Name: " << Name << endl;
+        cout << "Department: " << Department << endl;
+        cout << "Relation: " << Relation << endl;
+        cout << "Experience: " << Experience << endl;
+        cout << "Age: " << Age << endl; 
+    }
+    Employee(string name, string department, string relation, int experience, int age){
+        Name = name;
+        Department = department;
+        Relation = relation;
+        Experience = experience;
+        Age = age;
+    }
+    void askForPromotion(){
+        if(Age >= 18 && Relation == "Good" && Experience >= 3){
+            cout << Name << " deserves a promotion." << endl;
+        }else{
+            cout << "Sorry, " << Name << " does not deserve a promotion." << endl;
+        }
+    }
  };   
-int main(){}
+int main(){
+    Employee emp1 = Employee("Gorkem", "Mobile", "Good", 2, 27);
+    Employee emp2 = Employee("Furkan", "IT", "Good", 4, 29);
+    emp1.askForPromotion();
+    emp2.askForPromotion();
+}
